c31382cdbca7022815461aaa0910b9e2d49841a9
taufiq33 <taufiqhidayat1908@gmail.com>
2022-08-19 21:05:47 +0700

    modifikasi model order, link dengan invoice

--- app/order/model.js
+++ app/order/model.js
@@ -1,6 +1,8 @@
 const {model, Schema, default: mongoose} = require('mongoose');
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 
+const Invoice = require('../invoice/model');
+
 const OrderSchema = Schema({
   status: {
     type: String,
@@ -40,6 +42,23 @@ OrderSchema.virtual('items_count').get(function() {
   return this.order_items.reduce((total, item) => {
     return total + parseInt(item.qty)
   }, 0);
+})
+
+OrderSchema.post('save', async function() {
+    let sub_total = this.order_items.reduce((total, item) => {
+      return total += (item.price * item.qty)
+    }, 0);
+
+    let invoice = new Invoice({
+      user: this.user,
+      order: this._id,
+      sub_total,
+      delivery_fee: parseInt(this.delivery_fee),
+      total: parseInt(sub_total + this.delivery_fee),
+      delivery_address: this.delivery_address,
+    })
+
+    await invoice.save();
 })
 
 const Order = model('Order', OrderSchema);
